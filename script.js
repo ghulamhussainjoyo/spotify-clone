514696174dbfd1f22981b2259f18c0540942980e
ghulamhussainjoyo <ghulamhussain.kcsf18@iba-suk.edu.pk>
2023-08-28 11:44:26 +0500

    spotify modified

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+class LibrarySongs {
+
+
+    getSongRow() {
+        return `
+        <div class="row">
+        <div>
+            <img src="./assets/avatar.jpeg" alt="" />
+            <div>
+                <h5>24.06.23</h5>
+                <p>Hasancan ozmen</p>
+            </div>
+        </div>
+
+        <i class="ri-pushpin-fill"></i>
+        </div>
+        `;
+    }
+}
+
+
+
